34ebf588a319f3583dd59ff7cb83a5689d1757fc
MATUTINO <pimba@tecnologicoismac.edu.ec>
2023-11-17 10:11:58 -0500

    "Tercer cambio"

--- src/com/distribuida/principal/principal.java
+++ src/com/distribuida/principal/principal.java
@@ -5,6 +5,9 @@ public class principal {
 	public static void  main (String[] args) {
 	
 		System.out.println("HOLA MUNDO DESDE GIT!!!.....");
+		
+		
+		System.out.println("HOLA MUNDO DESDE GIT!!!.....");
 	}
 	
 }
